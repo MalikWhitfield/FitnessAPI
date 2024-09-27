41b0b757fd937428b3e24b8e443a048fed7a48ab
MalikWhitfield <malikwhitfield29@gmail.com>
2024-09-27 17:45:37 -0400

    create user works

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Persistence.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UsersController : BaseAPIController
+    {
+        private readonly IUserRepository _repository;
+        public UsersController(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public async Task<ActionResult> CreateUser([FromBody] User user)
+        {
+            await _repository.CreateUser(user);
+            return CreatedAtAction("CreateUser", new { id = user.Id }, user);
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
