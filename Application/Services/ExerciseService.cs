02ef1ae513b10029fa0dd735cd8d279175b2f7ed
MalikWhitfield <malikwhitfield29@gmail.com>
2024-09-29 14:19:16 -0400

    create exercise service and interface

---
+++ Application/Services/ExerciseService.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Models;
+using Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class ExerciseService : IExerciseService
+    {
+        private readonly IExerciseRepository _exerciseRepository;
+
+        public ExerciseService(IExerciseRepository exerciseRepository)
+        {
+            _exerciseRepository = exerciseRepository;
+        }
+
+        public async Task<List<Exercise>> GetAllExercises()
+        {
+            return await _exerciseRepository.GetExercises();
+        }
+
+        public async Task<Exercise> GetExerciseById(int id)
+        {
+            var exercise = await _exerciseRepository.GetExerciseById(id);
+            if (exercise == null)
+            {
+                throw new Exception("Exercise not found");
+            }
+            return exercise;
+        }
+
+    }
+}
---
+++ Application/Services/IExerciseService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models;
+
+namespace Application.Services
+{
+    public interface IExerciseService
+    {
+        Task<List<Exercise>> GetAllExercises();
+        Task<Exercise> GetExerciseById(int id);
+    }
+}
