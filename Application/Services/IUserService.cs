1a84c425636e5a24bf9cda503591501fb87796d4
MalikWhitfield <malikwhitfield29@gmail.com>
2024-09-29 14:19:05 -0400

    create user services and interface

---
+++ Application/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+
+
+namespace Application.Services
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetAllUsers();
+        Task<User> GetUserById(Guid id);
+        Task CreateUser(User user);
+        Task UpdateUser(User user);
+    }
+}
---
+++ Application/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Models;
+using Persistence.Repositories;
+
+namespace Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            return await _userRepository.GetUsers();
+        }
+
+        public async Task<User> GetUserById(Guid id)
+        {
+            var user = await _userRepository.GetUserById(id);
+            if (user == null)
+            {
+                throw new Exception("User not found");
+            }
+            return user;
+        }
+
+        public async Task CreateUser(User user)
+        {
+            await _userRepository.CreateUser(user);
+        }
+
+        public async Task UpdateUser(User user)
+        {
+            var existingUser = await _userRepository.GetUserById(user.Id);
+            if (existingUser == null)
+            {
+                throw new Exception("User not found");
+            }
+            await _userRepository.UpdateUser(user);
+        }
+    }
+}
