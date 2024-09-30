294cbb3f3b706062e92f5e9d2a4fce027edc1d0b
MalikWhitfield <malikwhitfield29@gmail.com>
2024-09-30 15:12:47 -0400

    create DTOs

---
+++ Domain/DTOs/ExerciseDTO.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enums;
+
+namespace Domain.DTOs
+{
+    public class ExerciseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ExerciseType ExerciseType { get; set; }
+        public MuscleGroup MuscleGroup { get; set; }
+    }
+}
---
+++ Domain/DTOs/UserExerciseDTO.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Domain.DTOs
+{
+    public class UserExerciseDTO
+    {
+        public int Id { get; set; }
+        public int ExerciseId { get; set; }
+        public Guid UserId { get; set; }
+        public int MaxWeight { get; set; }
+        public int BestTime { get; set; }
+        public ExerciseDTO Exercise { get; set; }
+        public UserDTO User { get; set; }
+    }
+}
